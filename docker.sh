755a2cdec7188d7d0cff3235b71cde2663d57e41
roblabla <unfiltered@roblab.la>
2023-03-15 19:47:46 +0000

    Better startup script

--- docker.sh
+++ docker.sh
@@ -3,4 +3,6 @@
 set -e
 
 docker load < $(nix-build --no-out-link default.nix)
+docker stop gollum || true
+docker rm gollum || true
 docker run --name gollum --rm -d -p 4567:4567 -v $(pwd):/wiki -v $HOME/.ssh/howtofish_ecdsa:/ssh_key gollum gollum --config /wiki/config.rb
