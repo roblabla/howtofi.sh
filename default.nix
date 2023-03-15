70d9c7932f5750d6bb402bffccf9f7a832c8a87a
roblabla <unfiltered@roblab.la>
2023-03-15 22:36:29 +0000

    Fix restructured text

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ dockerTools.buildImage {
   tag = "latest";
   copyToRoot = pkgs.buildEnv {
     name = "mygollum-root";
-    paths = [ pkgs.gollum pkgs.busybox ];
+    paths = [ pkgs.gollum pkgs.busybox (pkgs.python3.withPackages (ps: [ ps.docutils ])) ];
     pathsToLink = [ "/bin" ];
   };
 
