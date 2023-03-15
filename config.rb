5dacfcac126c8b42c9a010608faaa101e1e0ca7d
roblabla <unfiltered@roblab.la>
2023-03-15 19:49:28 +0000

    Fix github sync

--- config.rb
+++ config.rb
@@ -19,7 +19,7 @@ module Gollum
   end
 end
 
-credentials = Rugged::Credentials::SshKey(username: 'git', privatekey: '/ssh_key')
+credentials = Rugged::Credentials::SshKey.new(username: 'git', privatekey: '/ssh_key')
 Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
   committer.wiki.repo.git.pull('gh', committer.wiki.ref, credentials: credentials)
   committer.wiki.repo.git.push('gh', committer.wiki.ref, credentials: credentials)
